c2d4a8681792039e77bd6eabd8a58eddadd3c625
LanAleCoder <peter24alexander@gmail.com>
2022-04-14 20:47:08 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import React from 'react';
+import {useState} from 'react';
+
+function App() {
+  const[number, setNumber] = useState(0);
+  return (
+    <div className="App">
+        <div className="container__counter-father">
+      <div className="container__counter-child">
+        
+      <h1 className={number > 0 ? "positive" : number < 0 ? "negative" : null}>{number}</h1>
+      <div className="container__counter-buttons">        
+      <button type="button" className="container__counter-button increase" onClick={() => setNumber(number+1)}>+</button>
+      <button type="button" className="container__counter-button reset" onClick={() => setNumber(0)}>Restablecer</button>
+      <button type="button" className="container__counter-button decrease" onClick={() => setNumber(number-1)}>-</button>
+      </div>
+      </div>
+    </div>
+   </div>
+  );
+}
+
+function decrement() {
+  
+}
+
+function increment() {
+}
+
+
+
+export default App;
